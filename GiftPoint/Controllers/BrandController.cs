c7610b019c57567d5ccca915a2347342246ec0e5
asiframzan567 <asiframzan567@gmail.com>
2021-05-04 00:30:50 +0500

    dfdf

---
+++ GiftPoint/Controllers/BrandController.cs
@@ -0,0 +1,34 @@
+﻿using GiftPoint.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GiftPoint.Controllers
+{
+    public class BrandController : Controller
+    {
+        // GET: Brand
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Brand/Create
+        [AllowAnonymous]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Brand/Create
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Brand model)
+        {
+            return View();
+        }
+    }
+}
